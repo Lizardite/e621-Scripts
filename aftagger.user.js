a2bdcc26a0a274c221c88867783b212979f8487b
Lizardite Népouite <lizardite277@gmail.com>
2015-05-10 16:43:25 +0200

    Added custom tag box

--- aftagger.user.js
+++ aftagger.user.js
@@ -1,6 +1,6 @@
 // ==UserScript== 
 // @name          e621 AFTagger
-// @version       1.0
+// @version       1.1
 // @description   e621 quick anthro/feral tagger
 // @author        Lizardite
 
@@ -19,13 +19,6 @@
 var h = document.createElement("script");
 
 h.innerHTML = "(" + function() {
-	var options = [
-		{ text: "None", tags: "" },
-		{ text: "Anthro", tags: "anthro" },
-		{ text: "Feral", tags: "feral" }
-	];
-	var defaultOption = 1;
-
 	var createSidebarSection = function(title) {
 		var div = document.createElement("div");
 		div.style.marginBottom = "1em";
@@ -44,7 +37,7 @@ h.innerHTML = "(" + function() {
 		for (var optionId = 0; optionId < options.length; optionId++) {
 			var option = document.createElement("option");
 			option.value = optionId;
-			option.innerText = options[optionId].text;
+			option.innerText = options[optionId];
 			if (defaultOption == optionId) {
 				option.selected = 1;
 			}
@@ -66,19 +59,47 @@ h.innerHTML = "(" + function() {
 		return false;
 	}
 
-	var posts = document.getElementsByClassName("thumb");
-	for (var i = 0; i < posts.length; i++) {
-		var score = posts[i].getElementsByClassName("post-score")[0];
-		score.remove();
-		score.style.width = "100%";
-		posts[i].appendChild(score);
+	var removeSelectors = function() {
+		var oldSelectors = document.getElementsByClassName("mtt-posttype");
+		while (oldSelectors.length) {
+			oldSelectors[0].remove();
+		}
+	}
 
-		var selector = buildSelector(options, defaultOption);
-		selector.className = "mtt-posttype";
-		score.appendChild(selector);
+	var createSelectors = function(tags) {
+		var posts = document.getElementsByClassName("thumb");
+
+		tags = "---\n" + tags;
+		tags = tags.trim().replace(/[\t ]+/g, " ").replace(/\n+/g, "\n").split("\n");
+
+		for (var i = 0; i < posts.length; i++) {
+			var score = posts[i].getElementsByClassName("post-score")[0];
+			score.remove();
+			score.style.width = "100%";
+			posts[i].appendChild(score);
+
+			var selector = buildSelector(tags, tags.length == 1 ? 0 : 1);
+			selector.className = "mtt-posttype";
+			score.appendChild(selector);
+		}
 	}
 
 	var sendHolder = createSidebarSection("AFTagger");
+
+	var tags = Cookie.get("aftagger-tags");
+	if (!tags) {
+		tags = "anthro\nferal\nhuman hybrid";
+	}
+	createSelectors(tags);
+
+	var tagsBox = document.createElement("textarea");
+	tagsBox.value = tags;
+	tagsBox.onchange = function(e) {
+		removeSelectors();
+		createSelectors(e.target.value);
+		Cookie.put("aftagger-tags", e.target.value);
+	}
+	sendHolder.appendChild(tagsBox);
 
 	var sendButton = document.createElement("button");
 	sendButton.onclick = function(e) {
